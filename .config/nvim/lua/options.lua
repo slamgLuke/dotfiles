87079a870722a7ce44102f9156070eeba82f5794
slamgLuke <lucascarranzab2@gmail.com>
2024-05-17 18:37:16 -0500

    2 indent for lua

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,9 +6,19 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 local opt = vim.opt
-
 opt.tabstop = 4
 opt.shiftwidth = 4
+opt.softtabstop = 4
 opt.expandtab = true
 opt.smartindent = true
 opt.autoindent = true
+
+local api = vim.api
+api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    opt.tabstop = 2
+    opt.shiftwidth = 2
+    opt.softtabstop = 2
+  end,
+})
